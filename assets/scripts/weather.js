5da1a20dc27986c69288cab72c624d8e53fcbe6f
Maksyutov Vyacheslav <thelastslike@gmail.com>
2021-10-25 21:50:40 +0500

    feat:add weather translation

--- assets/scripts/weather.js
+++ assets/scripts/weather.js
@@ -6,18 +6,35 @@ const weatherDescription = document.querySelector('.weather-description');
 const city = document.querySelector('.city');
 
 
+function defCity(){
+let lang = getLanguage();
+if(lang=='en'){
+    city.value = 'Minsk';
+}else{
+    city.value= 'Минск';
+}
+}
+
+
+
 
 
 async function getWeather() {
+    let lang = getLanguage();
 try{
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=c2dc4259e997002f850ecc1579e37165&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=${lang}&appid=c2dc4259e997002f850ecc1579e37165&units=metric`;
     const res = await fetch(url);
     const data = await res.json();
     weatherIcon.className = 'weather-icon owf';
     weatherIcon.classList.add(`owf-${data.weather[0].id}`);
     temperature.textContent = `${Math.floor(data.main.temp)}°C`;
+    if(lang=='en'){
     wind.textContent=`Wind speed: ${Math.floor(data.wind.speed)} m/s`
     humidity.textContent=`Humidity: ${Math.floor(data.main.humidity)} %`
+    }else{
+    wind.textContent=`Скорость ветра: ${Math.floor(data.wind.speed)} м/с`
+    humidity.textContent=`Влажность: ${Math.floor(data.main.humidity)} %`
+    }
     weatherDescription.textContent = data.weather[0].description;
 }
 catch(eror){
@@ -44,6 +61,7 @@ function getLocalStorage(){
 }   
 }
 window.addEventListener('load',getLocalStorage);
-city.addEventListener('change',getWeather);
+checkbox.addEventListener('change',defCity);
+checkbox.addEventListener('change',getWeather);
 getWeather()
 
